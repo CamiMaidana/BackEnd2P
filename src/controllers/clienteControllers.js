7026a15e6135599615ca83dd94c5931e923668c2
Laura Capdevila <88208172+lauricapdevila@users.noreply.github.com>
2023-05-17 22:05:28 -0400

    Update clienteControllers.js
    Faltó añadir PUT y DELETE Clientes

--- src/controllers/clienteControllers.js
+++ src/controllers/clienteControllers.js
@@ -10,3 +10,17 @@ export const postClientes = async(req,res) =>{
     const cliente = await Cliente.create({cedula,nombre,apellido})
     res.json(cliente)
 }
+//PUT Clientes
+export const putClientes = async(req,res) =>{
+    const {cedula,nombre,apellido} = req.body
+    const {id} = req.params
+    const cliente = await Cliente.update({cedula,nombre,apellido}, {where: {id}})
+    res.json(cliente)
+}
+
+//DELETE Clientes
+export const deleteClientes = async(req,res) =>{
+    const {id} = req.params
+    const cliente = await Cliente.destroy({where: {id}})
+    res.json(cliente)
+}
