01c82b370c8c9861b744c8d462c34bee92e81ca3
CamiMaidana <camimai1.cm@fpuna.edu.py>
2023-05-22 14:39:00 -0400

    frontend funcional 	modified:   db.sqlite3 	modified:   frontend/Restaurantes/Restaurantes.html 	modified:   frontend/Restaurantes/Restaurantes.js

--- frontend/Restaurantes/Restaurantes.js
+++ frontend/Restaurantes/Restaurantes.js
@@ -1,6 +1,6 @@
 //fetch
 //document.getElementById
-
+/*
 const API_URL = "http://localhost:3000/Restaurante"
 
 
@@ -42,4 +42,87 @@ function crearFilaRestaurante(Restaurante){
     return fila
 }
 
-mostrarTabla()
+mostrarTabla()*/
+// Obtener una referencia al formulario
+
+// Obtener una referencia al formulario
+const formulario = document.getElementById('formulario');
+
+// Escuchar el evento submit del formulario
+formulario.addEventListener('submit', async (event) => {
+    event.preventDefault(); // Prevenir la acción por defecto del formulario
+
+    // Obtener los valores de los campos del formulario
+    const nombre = document.getElementById('nombreRestaurante').value;
+    const direccion = document.getElementById('direccionRestaurante').value;
+
+    try {
+        // Enviar la solicitud POST al backend
+        const response = await fetch('http://localhost:3000/restaurante', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ nombre, direccion })
+        });
+
+        if (response.ok) {
+            // El restaurante se agregó exitosamente
+            const restaurante = await response.json();
+            console.log('Restaurante agregado:', restaurante);
+            formulario.reset();
+        } else {
+            // Ocurrió un error al agregar el restaurante
+            console.error('Error al agregar el restaurante');
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+// Obtén una referencia al tbody de la tabla
+const tablaBody = document.getElementById('tabla-body');
+
+// Función para cargar la tabla con los datos de los restaurantes
+const cargarTablaRestaurantes = async () => {
+    try {
+        // Realiza una solicitud GET para obtener los datos de los restaurantes desde el backend
+        const response = await fetch('http://localhost:3000/restaurante');
+        const data = await response.json();
+
+        // Limpia el contenido actual del tbody
+        tablaBody.innerHTML = '';
+
+        // Recorre los datos de los restaurantes y crea las filas de la tabla
+        data.forEach(restaurante => {
+            const fila = document.createElement('tr');
+
+            // Crea las celdas de la fila con los datos del restaurante
+            const celdaId = document.createElement('td');
+            celdaId.textContent = restaurante.id;
+            fila.appendChild(celdaId);
+
+            const celdaNombre = document.createElement('td');
+            celdaNombre.textContent = restaurante.nombre;
+            fila.appendChild(celdaNombre);
+
+            const celdaDireccion = document.createElement('td');
+            celdaDireccion.textContent = restaurante.direccion;
+            fila.appendChild(celdaDireccion);
+
+            const celdaAcciones = document.createElement('td');
+            // Agrega cualquier lógica adicional para las acciones (botones, enlaces, etc.)
+            // Aquí puedes agregar botones para editar o eliminar el restaurante si lo deseas
+            fila.appendChild(celdaAcciones);
+
+            // Agrega la fila a la tabla
+            tablaBody.appendChild(fila);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Llama a la función para cargar la tabla al cargar la página
+window.addEventListener('DOMContentLoaded', cargarTablaRestaurantes);
+
